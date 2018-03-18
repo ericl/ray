d62297de236b532cff47c1d7e5cc7b6255d46a8c
Eric Liang <ekhliang@gmail.com>
2018-03-18 16:15:19 -0700

    Sun Mar 18 16:15:19 PDT 2018

--- python/ray/rllib/learn_il.py
+++ python/ray/rllib/learn_il.py
@@ -2,42 +2,77 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import os
+import json
+
 import numpy as np
 import gym
 import tensorflow as tf
 
+import ray
 from ray.rllib.models.action_dist import Categorical
 from ray.rllib.models.fcnet import FullyConnectedNetwork
 from ray.rllib.cartpole import MakeCartpoleHarder
+from ray.tune import run_experiments, register_trainable, grid_search
 
-N = 500
+PATH = os.path.expanduser("~/Desktop/cartpole-expert.json")
 
-observations = tf.placeholder(tf.float32, [None, N + 4])
-expert_actions = tf.placeholder(tf.int32, [None])
-network = FullyConnectedNetwork(observations, 2, {})
-action_dist = Categorical(network.outputs)
 
-loss = -tf.reduce_mean(action_dist.logp(expert_actions))
-optimizer = tf.train.AdamOptimizer()
-train_op = optimizer.minimize(loss)
+def train(config, reporter):
+    N = config.get("N", 500)
+    BATCH_SIZE = config.get("batch_size", 128)
 
-env = gym.make("CartPole-v0")
-preprocessor = MakeCartpoleHarder(env.observation_space, {
-    "custom_options": {
-        "seed": 0,
-        "noise_size": N,
-        "matrix_size": N,
-        "invert": False,
-    },
-})
+    observations = tf.placeholder(tf.float32, [None, N + 4])
+    expert_actions = tf.placeholder(tf.int32, [None])
+    network = FullyConnectedNetwork(observations, 2, config.get("model", {}))
+    action_dist = Categorical(network.outputs)
 
-sess = tf.Session()
-sess.run(tf.global_variables_initializer())
-for i in range(100):
-    cur_loss, _ = sess.run([loss, train_op], feed_dict={
-        observations: [
-            preprocessor.transform([1, 2, 2, 1]),
-            preprocessor.transform([1, 2, 3, 4])],
-        expert_actions: [0, 1],
+    loss = -tf.reduce_mean(action_dist.logp(expert_actions))
+    optimizer = tf.train.AdamOptimizer()
+    train_op = optimizer.minimize(loss)
+
+    env = gym.make("CartPole-v0")
+    preprocessor = MakeCartpoleHarder(env.observation_space, {
+        "custom_options": {
+            "seed": 0,
+            "noise_size": N,
+            "matrix_size": N,
+            "invert": False,
+        },
     })
-    print("iteration {} accuracy {}".format(i, np.exp(-cur_loss)))
+
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+    data = [json.loads(x) for x in open(PATH).readlines()]
+    print("preprocessing data")
+    for t in data:
+        t["obs"] = preprocessor.transform(t["obs"])
+
+    print("start training")
+    for i in range(1000):
+        losses = []
+        for _ in range(len(data) // BATCH_SIZE):
+            batch = np.random.choice(data, BATCH_SIZE)
+            cur_loss, _ = sess.run([loss, train_op], feed_dict={
+                observations: [t["obs"] for t in batch],
+                expert_actions: [t["action"] for t in batch],
+            })
+            losses.append(cur_loss)
+        acc = np.mean([np.exp(-l) for l in losses])
+        reporter(timesteps_total=i, mean_accuracy=acc)
+
+
+ray.init()
+register_trainable("il", train)
+run_experiments({
+    "il": {
+        "run": "il",
+        "config": {
+            "N": 500,
+            "model": {
+                "fcnet_activation": "relu",
+                "fcnet_hiddens": [256, 256, 8],
+            },
+        },
+    }
+})
