b4e307ba759a0d6cd43f23f2996589a1f5f5fb19
Eric Liang <ekhliang@gmail.com>
2018-03-27 15:27:07 -0700

    add fixed bg mode

--- python/ray/rllib/render_cartpole.py
+++ python/ray/rllib/render_cartpole.py
@@ -10,16 +10,45 @@ k = 0
 if not os.path.exists("/tmp/cartpole"):
     os.makedirs("/tmp/cartpole")
 
+BACKGROUND = np.tile(
+    np.concatenate([np.arange(200, 0, -1), np.arange(0, 200, 1)]),
+    (80, 1))
+
+BACKGROUND_4XWIDE = np.tile(
+    np.concatenate([
+        np.arange(200, 0, -1),
+        np.arange(0, 200, 1),
+        np.arange(200, 0, -1),
+        np.arange(0, 200, 1),
+        np.arange(200, 0, -1),
+        np.arange(0, 200, 1),
+        np.arange(200, 0, -1),
+        np.arange(0, 200, 1)]),
+    (80, 1))
+
 def render_frame(obs, env_config):
     cart_pos = obs[0]
     cart_velocity = obs[1]
     pole_angle = obs[2]
     angle_velocity = obs[3]
+    angle_multiple = 1
     w = 80  # screen width
+    xpos = cart_pos / 2.4 * w/2 + w/2
     if env_config["background"] == "noise":
         canvas = np.random.randint(200, size=(w, w), dtype=np.uint8)
     elif env_config["background"] == "zeros":
         canvas = np.zeros((w, w), dtype=np.uint8)
+    elif env_config["background"] in ["fixed", "fixed_noisy"]:
+        angle_multiple = 3
+        xpos = w/2
+        xs = int(cart_pos / 2.4 * 160 + 160)
+        xw = int(cart_pos / 2.4 * 760 + 760)
+        canvas = np.copy(BACKGROUND[:, xs:xs+80]).astype(np.float64)
+        wide = np.copy(BACKGROUND_4XWIDE[:, xw:xw+80]).astype(np.float64)
+        canvas[0:10,:] = wide[0:10,:]
+        if env_config["background"] == "fixed_noisy":
+            canvas = np.maximum(canvas, np.random.randint(200, size=(w, w)))
+        canvas = canvas.astype(np.int64)
     else:
         assert False, env_config
 
@@ -27,7 +56,6 @@ def render_frame(obs, env_config):
     canvas[:, 0] = 100
     canvas[w-1, :] = 100
     canvas[:, w-1] = 100
-    xpos = cart_pos / 2.4 * w/2 + w/2
     c = 5  # cart width
     left_aa_value = int((1 - xpos % 1) * 255)
     right_aa_value = int(xpos % 1 * 255)
@@ -35,8 +63,8 @@ def render_frame(obs, env_config):
     xpos = int(np.clip(np.ceil(xpos), 0, w-1))
 
     rr, cc = polygon(
-        (0, w, w, 0),
-        (w-12, w-12, w, w))
+        (1, w-1, w-1, 1),
+        (w-12, w-12, w-1, w-1))
     canvas[cc, rr] = 0
 
     # draw antialiased border
@@ -58,8 +86,8 @@ def render_frame(obs, env_config):
     error = None
     for pole_length in [60, 30, 20, 10, 5, 0]:
         try:
-            top_x = xpos + int(pole_length * np.sin(pole_angle))
-            top_y = w-10 - int(pole_length * np.cos(pole_angle))
+            top_x = xpos + int(pole_length * np.sin(angle_multiple * pole_angle))
+            top_y = w-10 - int(pole_length * np.cos(angle_multiple * pole_angle))
             rr, cc, val = line_aa(
                 top_x, top_y, xpos, w-10)
             canvas[cc, rr] = val * 255
@@ -80,15 +108,18 @@ def render_frame(obs, env_config):
 
 if __name__ == '__main__':
     lines = []
-    for line in open(os.path.expanduser("~/Desktop/cartpole-random.json")).readlines():
+    for line in open(os.path.expanduser("~/Desktop/cartpole-expert.json")).readlines():
         lines.append(json.loads(line))
         if len(lines) > 1000:
             break
 
     prev = None
+    ct = 0
     for i, line in enumerate(lines):
-        canvas = render_frame(line["obs"], {"background": "noise"}).squeeze()
+        canvas = render_frame(line["obs"], {"background": "fixed_noisy"}).squeeze()
         if prev == canvas.tolist():
             print("WARNING, similar obs", i)
+            ct += 1
         prev = canvas.tolist()
         imsave(os.path.expanduser("~/Desktop/render/{}.png").format(k), canvas)
+    print("Num warnings", ct)
