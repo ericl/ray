bfa57c34e11752d93f72143dc29bc30f22ae9d3b
Eric Liang <ekhliang@gmail.com>
2018-04-12 21:44:03 -0700

    add carracing

---
+++ python/ray/rllib/carracing_discrete/env_test.py
@@ -0,0 +1,33 @@
+import gym
+import ray
+from ray.tune.registry import register_env
+from ray.tune import run_experiments
+from ray.rllib import a3c
+from atari_wrappers import NoopResetEnv, WarpFrame, FrameStack
+from wrapper import DiscreteCarRacing
+
+def build_racing_env(_):
+    env = gym.make('CarRacing-v0')
+    env = DiscreteCarRacing(env)
+    env = NoopResetEnv(env)
+    env.override_num_noops = 50
+    env = WarpFrame(env, 80)
+    env = FrameStack(env, 4)
+    # hack needed to fix rendering on CarRacing-v0
+    env = gym.wrappers.Monitor(env, "/tmp/rollouts", force=True)
+    return env
+
+env_creator_name = "discrete-carracing-v0"
+register_env(env_creator_name, build_racing_env)
+
+ray.init()
+run_experiments({
+        "demo": {
+            "run": "A3C",
+            "env": "discrete-carracing-v0",
+            "config": {
+                "num_workers": 1
+            }
+        },
+    })
+
