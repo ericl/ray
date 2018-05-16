1cdb3f26833d48ed7fabeb1a0386b5ef5686dffe
Eric Liang <ekhliang@gmail.com>
2018-05-16 02:11:21 -0700

    invert bg

--- python/ray/rllib/render_car.py
+++ python/ray/rllib/render_car.py
@@ -26,7 +26,7 @@ def add_car_snow(obs, num_snow=10, noise=False):
         obs = obs / 2
         obs = obs + noise_vec
     if num_snow:
-        obs = obs / 2
+        obs = 255 - obs
         obs = create_snow_dynamic(num_snow, obs, 9, snow_size=8, intensities=[100, 200])
         if random.random() < .001:
             imsave("/tmp/snow_sample-{}-{}-{}.png".format(num_snow, noise, num_rendered), obs.squeeze())
