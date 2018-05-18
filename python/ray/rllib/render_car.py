48cf76774b50d0ca8a79eaf57243ffae4a7738e5
Eric Liang <ekhliang@gmail.com>
2018-05-17 17:14:29 -0700

    revert

--- python/ray/rllib/render_car.py
+++ python/ray/rllib/render_car.py
@@ -27,7 +27,7 @@ def add_car_snow(obs, num_snow=30, noise=False):
         obs = obs + noise_vec
     if num_snow:
 #        obs = 255 - obs
-        obs = create_snow_dynamic(num_snow, obs, 9, snow_size=5, intensities=[0, 250])
+        obs = create_snow_dynamic(num_snow, obs, 9, snow_size=8, intensities=[100, 200])
         if random.random() < .001:
             imsave("/tmp/snow_sample-{}-{}-{}.png".format(num_snow, noise, num_rendered), obs.squeeze())
     return obs
