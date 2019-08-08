4e31d74fbe5252fd4cb7004eaf9991b4493c9591
Eric Liang <ekhliang@gmail.com>
2019-08-08 15:53:35 -0700

    wip

--- ci/long_running_tests/start_workloads.sh
+++ ci/long_running_tests/start_workloads.sh
@@ -8,6 +8,7 @@ pushd "$ROOT_DIR"
 # Substitute in the appropriate Ray version and commit in the config file and
 # store it in a temporary file.
 CLUSTER_CONFIG="config.yaml"
+MULTI_CLUSTER_CONFIG="config-multi-node.yaml"
 
 if grep -q RAY_WHEEL_TO_TEST_HERE $CLUSTER_CONFIG; then
     echo "You must replace the RAY_WHEEL_TO_TEST_HERE string in $CLUSTER_CONFIG."
@@ -28,6 +29,20 @@ if [ $status != 0 ]; then
     echo "Some update processes failed with $status"
     exit 1
 fi
+
+# Start the multi-node workloads running.
+for workload_file in "$ROOT_DIR"/multi_node_workloads/*; do
+  file_name=$(basename -- "$workload_file")
+  workload_name="${file_name%.*}"
+  (
+      # Copy the workload to the cluster.
+      ray rsync_up $MULTI_CLUSTER_CONFIG --cluster-name="$workload_name" "$workload_file" "$file_name"
+      # Clean up previous runs if relevant.
+      ray exec $MULTI_CLUSTER_CONFIG --cluster-name="$workload_name" "source activate tensorflow_p36 && ray stop; rm -r /tmp/ray; tmux kill-server | true"
+      # Start the workload.
+      ray exec $MULTI_CLUSTER_CONFIG --cluster-name="$workload_name" "source activate tensorflow_p36 && python $file_name" --tmux
+   ) &
+done
 
 # Start the workloads running.
 for workload_file in "$ROOT_DIR"/workloads/*; do
